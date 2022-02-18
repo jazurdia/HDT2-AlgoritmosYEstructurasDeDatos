05b0af4ca3a823bfb8344188d7b4f41fc9d04d45
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-02-18 11:29:35 -0600

    Create StackAL.java

---
+++ StackAL.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class StackAL<T> implements IStack<T>{
+
+    private ArrayList<T> pila;
+
+    public StackAL(){
+        pila = new ArrayList<T>();
+
+    }
+
+    @Override
+    public void push(T value) {
+        // TODO Auto-generated method stub
+        pila.add(0, value);
+        
+    }
+
+    @Override
+    public T pull() {
+        return pila.remove(0);
+    }
+
+    @Override
+    public T peek() {
+        pila.get(0);
+        return null;
+    }
+
+    @Override
+    public int count() {
+        return pila.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return pila.isEmpty();
+    }
+    
+}
