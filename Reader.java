382d90dc4ddd26a86ad780aa9297a68ef27c3d3d
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-02-18 16:05:41 -0600

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,20 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reader {
-    try {
-        File file = new File("filename.txt");
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String data = scanner.nextLine();
-            System.out.println(data);
-        }
-        scanner.close();
-    }catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-        }
+    String filepath;
+
+    /**
+     * Constructor of class reader. 
+     * @param filepath the filepath in a string, so that the code knows what to read. 
+     */
+    public Reader(String filepath){
+        this.filepath = filepath;
     }
+
+    /**
+     * readFile process the file, given the filepath in the constructor of the class. 
+     * @return ArrayList containing the data that the calculator uses. 
+     */
+    public StackAL<String> readFile(){
+        StackAL<String> rawdata = new StackAL<String>();
+        try {
+            File file = new File(filepath);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                //String data = scanner.nextLine();
+                rawdata.push(scanner.nextLine());
+            }
+            scanner.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("Ha ocurrido un error. ");
+        }
+        return rawdata;
+
+    }
+    
     
 }
