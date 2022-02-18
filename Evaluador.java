1ef584b5b24838398cdedf1d547d1bf157399018
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-02-18 15:13:20 -0600

    Update Evaluador.java
    Class Evaluador now does arithmetic operations.

--- Evaluador.java
+++ Evaluador.java
@@ -1,9 +1,47 @@
-public class Evaluador implements IPosfixCalc {
+public class Evaluador implements IposFixCalc {
+
 
     @Override
     public int Evaluate(String expresion) {
-        // TODO Auto-generated method stub
-        return 0;
+
+        String[] parts = expresion.split(" ");
+        int numberA = 0;
+        int numberB = 0;
+        int ans = 0;
+        
+        try {
+            numberA = Integer.parseInt(parts[0]);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            System.out.println("Ha ocurrido un error. ");
+        }
+
+        try {
+            numberB = Integer.parseInt(parts[1]);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            System.out.println("Ha ocurrido un error. ");
+        }
+
+        switch (parts[2]) {
+            case "+":
+                ans = numberA + numberB;
+                break;
+        
+            case "-":
+                ans = numberA - numberB;
+                break;
+
+            case "*":
+                ans = numberA * numberB;
+                break;
+            
+            case "/": 
+                ans = numberA/numberB;
+                break;
+        }
+
+        return ans;
     }
     
 }
