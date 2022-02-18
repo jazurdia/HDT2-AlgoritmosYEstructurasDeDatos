b36c94e2718463f97a9ab898a315c239e5a842c5
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-02-18 11:29:31 -0600

    Create IPosfixCalc.java

---
+++ IPosfixCalc.java
@@ -0,0 +1,9 @@
+public interface IPosfixCalc {
+
+	/***
+	 * This method is used to evaluate a postfix expression
+	 * @param expresion this is a single expression like: 1 2 + 4 * 3 +
+	 * @return the evaluation of the expression
+	 */
+	int Evaluate(String expresion);
+}
